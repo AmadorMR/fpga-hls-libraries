bf969f9a77e1ed8ce0c83459c54f7e6411d91aef
AmadorMR <amadormrafiq@gmail.com>
2023-06-16 12:12:58 +0200

    Added Img_enhancer.hpp and Img_enahncer folder under examples

---
+++ vision/examples/img_enhancer/img_enhancer_tb.cpp
@@ -0,0 +1,118 @@
+#include "../../include/vision.hpp"
+#include <opencv2/core/matx.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+
+using namespace std;
+using namespace cv;
+using namespace hls;
+using cv::Mat;
+using vision::Img;
+
+// #define SMALL_TEST_FRAME // for faster simulation.
+#ifdef SMALL_TEST_FRAME
+#define WIDTH 100
+#define HEIGHT 56
+#define INPUT_IMAGE "toronto_100x56.bmp"
+#else
+#define WIDTH 1920
+#define HEIGHT 1080
+#define INPUT_IMAGE "toronto_1080p.bmp"
+#endif
+#define SIZE (WIDTH * HEIGHT)
+
+// We have to create a top-level function here even though the function simply
+// calls vision::ImgEnhance. This is required by our CoSim flow.
+template <vision::PixelType PIXEL_T_I, vision::PixelType PIXEL_T_O,
+          unsigned H, unsigned W, vision::StorageType STORAGE_I,
+          vision::StorageType STORAGE_O, vision::NumPixelsPerCycle NPPC>
+void hlsImgEnhance(Img<PIXEL_T_I, H, W, STORAGE_I, NPPC> &ImgIn,
+              Img<PIXEL_T_O, H, W, STORAGE_O, NPPC> &ImgOut,
+              unsigned &common_const, unsigned &r_const,unsigned &g_const,
+              unsigned &b_const, unsigned &r_mean, unsigned &g_mean, unsigned &b_mean,
+              unsigned &r_var, unsigned &g_var, unsigned &b_var) {
+#pragma HLS function top
+    vision::ImgEnhance(ImgIn, ImgOut, common_const, r_const, g_const, b_const,
+    r_mean, g_mean, b_mean, r_var, g_var, b_var);
+}
+
+//  Use OpenCV's convertScaleAbs as reference.
+void cvImgEnhance(cv::Mat &InMat, cv::Mat &OutMat, unsigned alpha, 
+    unsigned beta) {
+    cv::convertScaleAbs(InMat, OutMat, alpha, beta);
+}
+
+int main() {
+    // Load image from file, using OpenCV's imread function.
+    Mat BGRInMat = cv::imread(INPUT_IMAGE, cv::IMREAD_COLOR);
+
+    // By default, OpenCV reads and write image in BGR format, so let's convert
+    // to the more traditional RGB format.
+    Mat RGBInMat;
+    cv::cvtColor(BGRInMat, RGBInMat, cv::COLOR_BGR2RGB);
+
+    /*
+     * Call the SmartHLS top-level function and the OpenCV reference function,
+     * and compare the 2 results.
+     */
+    // 1. SmartHLS result
+    // First, convert the OpenCV `Mat` into `Img`.
+    Img<vision::PixelType::HLS_8UC3, HEIGHT, WIDTH, vision::StorageType::FIFO,
+        vision::NPPC_1>
+        HlsInImg;
+    Img<vision::PixelType::HLS_8UC3, HEIGHT, WIDTH, vision::StorageType::FIFO,
+        vision::NPPC_1>
+        HlsOutImg;
+    convertFromCvMat(RGBInMat, HlsInImg);
+    // Then, call the SmartHLS top-level function.
+    /* PROVISIONAL: First we need to calculate the values of the const from 
+        the brightness and constrast values: 
+        the OpenCV funct has a form of: beta + alpha*f(i,j) 
+        really similar to our ImgEnhance but only capable of 
+        modifying all three RGB at the same time */
+    unsigned alpha = 1;
+    unsigned beta = 0;
+
+    unsigned hls_c_c = 1;
+
+    unsigned hls_r_c = 3;
+    unsigned hls_g_c = 1;
+    unsigned hls_b_c = 1;
+    unsigned r_mean;
+    unsigned g_mean;
+    unsigned b_mean;
+    unsigned r_var;
+    unsigned g_var;
+    unsigned b_var;
+    
+    /* TO DO: change manually and individually each const for RGB
+    and see if result changes in the .png*/
+    hlsImgEnhance(HlsInImg, HlsOutImg, hls_c_c, hls_r_c, hls_g_c, hls_b_c,
+                    r_mean, g_mean, b_mean, r_var, g_var, b_var);
+    // Finally, convert the `Img` back to OpenCV `Mat`.
+    Mat HlsOutMat;
+    convertToCvMat(HlsOutImg, HlsOutMat);
+
+    // 2. OpenCV result
+    Mat CvOutMat;
+    cvImgEnhance(RGBInMat, CvOutMat, alpha, beta);
+
+    // 4. Transform back to BGR before writting because imwrite uses BGR too
+    Mat CvOutMatBGR;
+    cv::cvtColor(CvOutMat, CvOutMatBGR, cv::COLOR_RGB2BGR);
+    Mat HlsOutMatBGR;
+    cv::cvtColor(HlsOutMat, HlsOutMatBGR, cv::COLOR_RGB2BGR);
+    // 3. Print the HlsOutMat and CvOutMat as pictures for reference.
+    cv::imwrite("hls_output.png", HlsOutMatBGR);
+    cv::imwrite("cv_output.png", CvOutMatBGR);
+
+    
+    // 4. Compare the SmartHLS result and the OpenCV result.
+    // Use this commented out line to report location of errors.
+    //   vision::compareMatAndReport<unsigned char>(HlsOutMat, CvOutMat, 0);
+    float ErrPercent = vision::compareMat(HlsOutMat, RGBInMat, 0);
+    printf("Percentage of over threshold: %0.2lf%\n", ErrPercent);
+
+    return ErrPercent;
+}
---
+++ vision/include/imgproc/img_enhancer.hpp
@@ -0,0 +1,170 @@
+// ©2022 Microchip Technology Inc. and its subsidiaries
+//
+// Subject to your compliance with these terms, you may use this Microchip
+// software and any derivatives exclusively with Microchip products. You are
+// responsible for complying with third party license terms applicable to your
+// use of third party software (including open source software) that may
+// accompany this Microchip software. SOFTWARE IS “AS IS.” NO WARRANTIES,
+// WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY
+// IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A
+// PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT,
+// SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE
+// OF ANY KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
+// MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE
+// FORESEEABLE.  TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP’S TOTAL
+// LIABILITY ON ALL CLAIMS LATED TO THE SOFTWARE WILL NOT EXCEED AMOUNT OF
+// FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE. MICROCHIP
+// OFFERS NO SUPPORT FOR THE SOFTWARE. YOU MAY CONTACT MICROCHIP AT
+// https://www.microchip.com/en-us/support-and-training/design-help/client-support-services
+// TO INQUIRE ABOUT SUPPORT SERVICES AND APPLICABLE FEES, IF AVAILABLE.
+
+#ifndef __SHLS_VISION_IMG_ENHANCER_HPP__
+#define __SHLS_VISION_IMG_ENHANCER_HPP__
+
+#include "../common/common.hpp"
+#include "../common/utils.hpp"
+#include <hls/ap_fixpt.hpp>
+#include <hls/ap_int.hpp>
+
+namespace hls {
+namespace vision {
+/*
+Image Enhancer: adjust brightness, contrast and color balance of input video feed. 
+*/
+/*TODO:
+ - Add support for various NPPC
+ - Check validity of Constants beign unsigned
+  */
+
+
+
+template <PixelType PIXEL_T, unsigned H, unsigned W, StorageType STORAGE = FIFO,
+    NumPixelsPerCycle NPPC = NPPC_1>
+void ImgEnhance(vision::Img<PIXEL_T, H, W, STORAGE, NPPC> &InImg,
+             vision::Img<PIXEL_T, H, W, STORAGE, NPPC> &OutImg,
+             unsigned &common_const, unsigned &r_const,unsigned &g_const,
+             unsigned &b_const, unsigned &r_mean, unsigned &g_mean, unsigned &b_mean,
+             unsigned &r_var, unsigned &g_var, unsigned &b_var) {
+    const unsigned ImgHeight = InImg.get_height(), ImgWidth = InImg.get_width();
+    OutImg.set_height(ImgHeight);
+    OutImg.set_width(ImgWidth);
+    const unsigned FrameSize = (ImgHeight * ImgWidth) / NPPC;
+
+    /*NEW: Could be hardcoded? Is log2 not realistic?*/
+    /*Log2 rounds down 20,89 => 20 so result mean is about half of real*/
+    /* 1080p=> 21 -- 2k => 22  --  4k => 23*/
+    /* const unsigned Pixels_to_avg = log2(FrameSize); */
+    const unsigned Pixels_to_avg = 21;
+
+    using PixelT = typename DT<PIXEL_T, NPPC>::T;
+    const unsigned ChannelWidth = DT<PIXEL_T, NPPC>::PerChannelPixelWidth;
+    using ChannelT = ap_int<ChannelWidth + 3>;
+
+
+    /*NEW =>*/
+    using ChannelSumT = ap_int<ChannelWidth * 4>;
+
+    ChannelSumT r_sum = ChannelSumT(0);
+    ChannelSumT g_sum = ChannelSumT(0);
+    ChannelSumT b_sum = ChannelSumT(0);
+/* 
+    ChannelSumT r_mean;
+    ChannelSumT g_mean;
+    ChannelSumT b_mean;
+
+    ChannelSumT r_var;
+    ChannelSumT g_var;
+    ChannelSumT b_var;  */
+
+    ChannelSumT r_absdiff = ChannelSumT(0);
+    ChannelSumT g_absdiff = ChannelSumT(0);
+    ChannelSumT b_absdiff = ChannelSumT(0);
+    /* <= NEW */
+    #pragma HLS loop pipeline
+    for (unsigned ImgIdx = 0; ImgIdx < FrameSize; ImgIdx++) {
+        PixelT InPixelWord = InImg.read(ImgIdx);
+        PixelT TmpPixelWord;
+
+        ChannelT r = InPixelWord.byte(0,ChannelWidth),
+                 g = InPixelWord.byte(1,ChannelWidth),
+                 b = InPixelWord.byte(2,ChannelWidth);
+        ChannelT    r_fixpt = r,
+                    g_fixpt = g,
+                    b_fixpt = b,
+                    r_const_fixpt = r_const,
+                    g_const_fixpt = g_const,
+                    b_const_fixpt = b_const,
+                    common_const_fixpt = common_const;
+        ChannelT r_out = common_const_fixpt + r_const_fixpt*r_fixpt,
+                 g_out = common_const_fixpt + g_const_fixpt*g_fixpt,
+                 b_out = common_const_fixpt + b_const_fixpt*b_fixpt;
+
+        /* 255 Value for 8 bits channel*/
+        const ChannelT MaxChannelVal = ChannelT(255);
+
+        r_out = (r_out > MaxChannelVal) ? MaxChannelVal : r_out;
+        g_out = (g_out > MaxChannelVal) ? MaxChannelVal : g_out;
+        b_out = (b_out > MaxChannelVal) ? MaxChannelVal : b_out;
+        
+        TmpPixelWord.byte(0,ChannelWidth) = r_out;
+        TmpPixelWord.byte(1,ChannelWidth) = g_out;
+        TmpPixelWord.byte(2,ChannelWidth) = b_out;
+
+        OutImg.write(TmpPixelWord,ImgIdx);
+
+        /*NEW =>*/
+        r_sum = r_sum + r_out;
+        g_sum = g_sum + g_out;
+        b_sum = b_sum + b_out; 
+        /* Mean only updated once per img? CL: 104 in vhdl*/
+        ChannelSumT r_abs = (r_mean-r_out < 0 ) ? r_out-r_mean : r_mean-r_out;
+        ChannelSumT g_abs = (g_mean-g_out < 0 ) ? g_out-g_mean : g_mean-g_out;
+        ChannelSumT b_abs = (b_mean-b_out < 0 ) ? b_out-b_mean : b_mean-b_out;
+        r_absdiff = r_absdiff + r_abs;
+        g_absdiff = g_absdiff + g_abs;
+        b_absdiff = b_absdiff + b_abs;
+/* 
+        if (ImgIdx == 1) {
+            std::cout << "r_sum_IDX960 = " << r_sum.to_string(10) << std::endl;
+            std::cout << "g_sum_IDX960 = " << g_sum.to_string(10) << std::endl;
+            std::cout << "b_sum_IDX960 = " << b_sum.to_string(10) << std::endl;
+            std::cout << "r_absdiff_IDX960 = " << r_absdiff.to_string(10) << std::endl;
+            std::cout << "g_absdiff_IDX960 = " << g_absdiff.to_string(10) << std::endl;
+            std::cout << "b_absdiff_IDX960 = " << b_absdiff.to_string(10) << std::endl;
+        }else if(ImgIdx == 1000){
+            std::cout << "r_sum_IDX0 = " << r_sum.to_string(10) << std::endl;
+            std::cout << "g_sum_IDX0 = " << g_sum.to_string(10) << std::endl;
+            std::cout << "b_sum_IDX0 = " << b_sum.to_string(10) << std::endl;
+            std::cout << "r_absdiff_IDX0 = " << r_absdiff.to_string(10) << std::endl;
+            std::cout << "g_absdiff_IDX0 = " << g_absdiff.to_string(10) << std::endl;
+            std::cout << "b_absdiff_IDX0 = " << b_absdiff.to_string(10) << std::endl;
+        } */
+        /* <= NEW*/
+    }
+    /*NEW*/
+    r_mean = r_sum >> Pixels_to_avg;
+    g_mean = g_sum >> Pixels_to_avg;
+    b_mean = b_sum >> Pixels_to_avg;
+
+    r_var = r_absdiff >> Pixels_to_avg;
+    g_var = g_absdiff >> Pixels_to_avg;
+    b_var = b_absdiff >> Pixels_to_avg;
+/* 
+    std::cout << "r_sum_Final = " << r_sum.to_string(10) << std::endl;
+    std::cout << "g_sum_Final = " << g_sum.to_string(10) << std::endl;
+    std::cout << "b_sum_Final = " << b_sum.to_string(10) << std::endl; */
+
+/*     std::cout << "r_mean = " << r_mean.to_string(10) << std::endl;
+    std::cout << "g_mean = " << g_mean.to_string(10) << std::endl;
+    std::cout << "b_mean = " << b_mean.to_string(10) << std::endl;
+
+    std::cout << "r_var = " << r_var.to_string(10) << std::endl;
+    std::cout << "g_var = " << g_var.to_string(10) << std::endl;
+    std::cout << "b_var = " << b_var.to_string(10) << std::endl; */
+
+
+}
+
+} // end of namespace vision
+} // end of namespace hls
+#endif 
